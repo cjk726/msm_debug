e9d2633da9edbb404c31be84bce8bd6485d6b5d7
Charles Krywko <ckrywko@chicagobooth.edu>
2017-07-25 01:09:35 +0000

    movie controller pruning

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params[:id])
+    @movie = Movie.find(params[:id])             # switched from "" to :
   end
 
   def new_form
@@ -12,36 +12,38 @@ class MoviesController < ApplicationController
 
   def create_row
     @movie = Movie.new
-    @movie.title = params["title"]
-    @movie.year = params["year"]
-    @movie.duration = params["duration"]
-    @movie.description = params["description"]
-    @movie.image_url = params["image_url"]
-    @movie.director_id = params["director_id"]
+    @movie.title = params[:title]                 # switched from "" to :
+    @movie.year = params[:year]                   # switched from "" to :
+    @movie.duration = params[:duration]           # switched from "" to :
+    @movie.description = params[:description]     # switched from "" to :
+    @movie.image_url = params[:image_url]         # switched from "" to :
+    @movie.director_id = params[:director_id]     # switched from "" to :
 
     @movie.save
-
-    redirect_to("/movies")
+  
+    render("show")
+    # redirect_to("/movies")                JD has render line above
   end
 
   def edit_form
-    @movie = Movie.find(params["id"])
+    @movie = Movie.find(params[:id])       # switched from "" to :
     
-    render("edit_form.html.erb")
+    # render("edit_form.html.erb")           not in JD's
   end
 
   def update_row
 
-    @movie = Movie.find(params["id"])
-    @movie.title = params["title"]
-    @movie.year = params["year"]
-    @movie.duration = params["duration"]
-    @movie.description = params["description"]
-    @movie.image_url = params["image_url"]
-    @movie.director_id = params["director_id"]
+    @movie = Movie.find(params["id"])             # switched from "" to :
+    @movie.title = params["title"]                # switched from "" to :
+    @movie.year = params["year"]                  # switched from "" to :
+    @movie.duration = params["duration"]          # switched from "" to :
+    @movie.description = params["description"]    # switched from "" to :
+    @movie.image_url = params["image_url"]        # switched from "" to :
+    @movie.director_id = params["director_id"]    # switched from "" to :
     @movie.save
     
-    redirect_to("/movies/"+@movie.id.to_s)
+    render("show")
+  #  redirect_to("/movies/"+@movie.id.to_s)        # JD has render line above
   end
 
   def destroy
